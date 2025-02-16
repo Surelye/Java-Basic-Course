dfeb18c51cd9e766fbbb9f1a2ba04db633714932
Artem_Borodin <Artem_Borodin@rntgroup.com>
2025-02-16 16:36:24 +0400

    Implemented Gradle Student CRUD-Service

---
+++ impl/src/main/java/by/borodin/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package by.borodin.repository;
+
+import by.borodin.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
